fa278b18be876c8139364983104c2e685c640824
keb656 <keb656@naver.com>
2018-06-07 00:46:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sally/seein/Config.java
@@ -0,0 +1,13 @@
+package com.example.sally.seein;
+
+/**
+ * Created by sally on 2018-06-01.
+ */
+
+public class Config {
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://203.153.148.32:80/Lire-SimpleApplication-1.0b4/FileUpload_test.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "http://203.153.148.32:80/Lire-SimpleApplication-1.0b4/uploads";
+}
